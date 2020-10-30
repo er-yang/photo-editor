174e92ec189c6217a178e65a5f2008daab933283
杨远财 <yangyuancai@cvte.com>
2020-10-30 20:40:20 +0800

    preview img in canvase

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,23 @@
-export function fromBlob(videofile: string) {
-  console.log(1);
+export function editor(file: ImageData) {
+  return file.height;
+}
+export function fromBlob(file: File) {
+  const url = file;
+  return url;
+}
+export function open(image: CanvasImageSource, container: HTMLElement) {
+
+  const canvas = document.createElement('canvas');
+  const div = document.createElement('div');
+  div.setAttribute('height', image.height + 'px');
+  div.setAttribute('width', image.width + 'px');
+  div.appendChild(canvas);
+  container.appendChild(div);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(image, 0, 0);
+  ctx.beginPath();
+  ctx.moveTo(20, 30);
+  ctx.arc(40, 50, 20, 0, 360);
+  ctx.stroke();
+  return canvas;
 }
